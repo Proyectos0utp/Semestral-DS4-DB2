e741c7754f9f8a9acafbcfa2c26df75c270cab9f
Proyectos0utp <92341252+Proyectos0utp@users.noreply.github.com>
2021-12-19 15:13:45 -0600

    Entidad Tema

---
+++ Semestral_DS4g/src/java/entidades/Tema.java
@@ -0,0 +1,43 @@
+package entidades;
+
+public class Tema {
+    
+    private String codTema;
+    private String tema;
+    private String imagen;
+    private String contenido;
+
+    public String getCodTema() {
+        return codTema;
+    }
+
+    public void setCodTema(String codTema) {
+        this.codTema = codTema;
+    }
+
+    public String getTema() {
+        return tema;
+    }
+
+    public void setTema(String tema) {
+        this.tema = tema;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    
+}
