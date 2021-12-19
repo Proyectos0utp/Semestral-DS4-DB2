b13175ce490e4b09e85c83fb660edf56c20de8f0
Proyectos0utp <92341252+Proyectos0utp@users.noreply.github.com>
2021-12-19 15:29:37 -0600

    Actualizacion metodo listar temas
    función if else
    ya que los grupos del 1 al 9 son de ciencias naturales, cod tema empieza en TCN y los grupos del 10 al 15 son de ciencias sociales, cod tema empieza en TCS

--- Semestral_DS4g/src/java/entidades/Grupo.java
+++ Semestral_DS4g/src/java/entidades/Grupo.java
@@ -18,121 +18,154 @@ import procesos.Conectar;
  * @author manue
  */
 public class Grupo {
-    
-    public static List<Usuario> generarListaEstudiantes(String codGrupo){
+
+    public static List<Usuario> generarListaEstudiantes(String codGrupo) {
         List<Usuario> lista = new ArrayList<>();
         Connection conn = null;
         Statement stmt = null;
         ResultSet rs = null;
-        
+
         try {
-            
+
             conn = Conectar.conectar();
             stmt = conn.createStatement();
             String query = "SELECT * FROM Estudiante WHERE cod_grupo ='" + codGrupo + "'";
             rs = stmt.executeQuery(query);
-            
-            while (rs.next()) {                
-                
+
+            while (rs.next()) {
+
             }
-            
+
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         } finally {
             Conectar.cerrarConexiones(conn, stmt, rs);
         }
-        
+
         return lista;
     }
-    
-    public static String buscarProfesor(String codGrupo){
+
+    public static String buscarProfesor(String codGrupo) {
         String nombre = "";
         Connection conn = null, conn2 = null;
         Statement stmt = null, stmt2 = null;
         ResultSet rs = null, rs2 = null;
-        
+
         try {
-            
+
             conn = Conectar.conectar();
             stmt = conn.createStatement();
             String query = "SELECT * FROM Grupo WHERE cod_grupo ='" + codGrupo + "'";
             rs = stmt.executeQuery(query);
-            
+
             if (rs.next()) {
-                
+
                 try {
                     conn2 = Conectar.conectar();
                     stmt2 = conn2.createStatement();
                     query = "SELECT * FROM Usuario WHERE correo='" + rs.getString("correo_maestro") + "'";
                     rs2 = stmt2.executeQuery(query);
-                    
+
                     if (rs2.next()) {
                         nombre = rs2.getString("nombre");
                     }
-                    
+
                 } catch (SQLException e) {
                     System.out.println(e.getMessage());
                 } finally {
                     Conectar.cerrarConexiones(conn2, stmt2, rs2);
                 }
-                
+
             }
-            
+
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         } finally {
             Conectar.cerrarConexiones(conn, stmt, rs);
         }
-        
+
         return nombre;
     }
-    
-    public static int buscarNivel(String codGrupo){
+
+    public static int buscarNivel(String codGrupo) {
         int nivel = -1;
         Connection conn = null;
         Statement stmt = null;
         ResultSet rs = null;
-        
+
         try {
-            
+
             conn = Conectar.conectar();
             stmt = conn.createStatement();
             String query = "SELECT * FROM Grupo WHERE cod_grupo ='" + codGrupo + "'";
             rs = stmt.executeQuery(query);
-            
+
             if (rs.next()) {
                 nivel = rs.getInt("cod_grupo");
             }
-            
+
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         } finally {
             Conectar.cerrarConexiones(conn, stmt, rs);
         }
-        
+
         return nivel;
     }
-    
-    public static List<Tema> generarListaTemas(String codGrupo){
+
+    public static List<Tema> generarListaTemas(String codGrupo) {
         List<Tema> lista = new ArrayList<>();
         Connection conn = null;
         Statement stmt = null;
         ResultSet rs = null;
-        
+
         try {
-            
+
             conn = Conectar.conectar();
             stmt = conn.createStatement();
-            String query, codTema, tableRows="";
+            String query, codTema, tableRows = "";
             Tema tema = new Tema();
-            
+
+            if (codGrupo.equals("01") || codGrupo.equals("02") || codGrupo.equals("03") || codGrupo.equals("04") || codGrupo.equals("05") || codGrupo.equals("06") || codGrupo.equals("07") || codGrupo.equals("08") || codGrupo.equals("09")) {
+                for (int i = 0; i < 4; i++) {
+                    codTema = "TCN" + codGrupo + "-" + (i + 1);
+                    query = "SELECT * FROM Tema WHERE cod_tema = '" + codTema + "'";
+                    rs = stmt.executeQuery(query);
+                    while (rs.next()) {
+                        tema.setCodTema(codTema);
+                        tema.setTema(rs.getString("tema"));
+                        tema.setImagen(rs.getString("imagen"));
+                        tema.setContenido(rs.getString("Contenido"));
+                        lista.add(tema);
+                    }
+                }
+            } else {
+                for (int i = 0; i < 4; i++) {
+                    codTema = "TCS" + codGrupo + "-" + (i + 1);
+                    query = "SELECT * FROM Tema WHERE cod_tema = '" + codTema + "'";
+                    rs = stmt.executeQuery(query);
+                    while (rs.next()) {
+                        tema.setCodTema(codTema);
+                        tema.setTema(rs.getString("tema"));
+                        tema.setImagen(rs.getString("imagen"));
+                        tema.setContenido(rs.getString("Contenido"));
+                        lista.add(tema);
+                    }
+                }
+            }
+
+            for (int i = 0; i < lista.size(); i++) {
+                tableRows += (lista.get(i)) + "<tr><td style=\"color: rgb(0,0,0);\">" + lista.get(i).getTema() + "</td><td><button class=\"btn btn-primary border rounded-0\" type=\"button\" style=\"color: rgb(0,0,0);background: rgba(255,255,255,0);border-color: rgb(0,0,0);width: 150px;margin-bottom: 30px;\">Administrar</button></td></tr>";
+            }
+
+            /*
             switch (codGrupo) {
                 case "01":
-                    for(int i=0; i<4; i++){
-                        codTema = "TCN01-"+(i+1);
+                    for (int i = 0; i < 4; i++) {
+                        codTema = "TCN" + codGrupo + "-" + (i + 1);
                         query = "SELECT * FROM Tema WHERE cod_tema = '" + codTema + "'";
                         rs = stmt.executeQuery(query);
-                        while (rs.next()) {    
+                        while (rs.next()) {
                             tema.setCodTema(codTema);
                             tema.setTema(rs.getString("tema"));
                             tema.setImagen(rs.getString("imagen"));
@@ -140,14 +173,38 @@ public class Grupo {
                             lista.add(tema);
                         }
                     }
-                    
+
                     for (int i = 0; i < lista.size(); i++) {
-                        tableRows += (lista.get(i)) + "<tr><td style=\"color: rgb(0,0,0);\">"+lista.get(i).getTema()+"</td><td><button class=\"btn btn-primary border rounded-0\" type=\"button\" style=\"color: rgb(0,0,0);background: rgba(255,255,255,0);border-color: rgb(0,0,0);width: 150px;margin-bottom: 30px;\">Administrar</button></td></tr>";
+                        tableRows += (lista.get(i)) + "<tr><td style=\"color: rgb(0,0,0);\">" + lista.get(i).getTema() + "</td><td><button class=\"btn btn-primary border rounded-0\" type=\"button\" style=\"color: rgb(0,0,0);background: rgba(255,255,255,0);border-color: rgb(0,0,0);width: 150px;margin-bottom: 30px;\">Administrar</button></td></tr>";
                     }
                     break;
                 case "02":
+                    for (int i = 0; i < 4; i++) {
+                        codTema = "TCN02-" + (i + 1);
+                        query = "SELECT * FROM Tema WHERE cod_tema = '" + codTema + "'";
+                        rs = stmt.executeQuery(query);
+                        while (rs.next()) {
+                            tema.setCodTema(codTema);
+                            tema.setTema(rs.getString("tema"));
+                            tema.setImagen(rs.getString("imagen"));
+                            tema.setContenido(rs.getString("Contenido"));
+                            lista.add(tema);
+                        }
+                    }
                     break;
                 case "03":
+                    for (int i = 0; i < 4; i++) {
+                        codTema = "TCN02-" + (i + 1);
+                        query = "SELECT * FROM Tema WHERE cod_tema = '" + codTema + "'";
+                        rs = stmt.executeQuery(query);
+                        while (rs.next()) {
+                            tema.setCodTema(codTema);
+                            tema.setTema(rs.getString("tema"));
+                            tema.setImagen(rs.getString("imagen"));
+                            tema.setContenido(rs.getString("Contenido"));
+                            lista.add(tema);
+                        }
+                    }
                     break;
                 case "04":
                     break;
@@ -176,39 +233,39 @@ public class Grupo {
                 default:
                     break;
             }
-            
+             */
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         } finally {
             Conectar.cerrarConexiones(conn, stmt, rs);
         }
-        
+
         return lista;
     }
-    
-    public static List<Tema> busqueda(String Busqueda){
+
+    public static List<Tema> busqueda(String Busqueda) {
         List<Tema> lista = new ArrayList<>();
         Connection conn = null;
         Statement stmt = null;
         ResultSet rs = null;
-        
+
         try {
-            
+
             conn = Conectar.conectar();
             stmt = conn.createStatement();
             String query = "SELECT * FROM Tema WHERE tema ='" + Busqueda + "'";
             rs = stmt.executeQuery(query);
-            
-            while (rs.next()) {                
-                
+
+            while (rs.next()) {
+
             }
-            
+
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         } finally {
             Conectar.cerrarConexiones(conn, stmt, rs);
         }
-        
+
         return lista;
     }
 }
